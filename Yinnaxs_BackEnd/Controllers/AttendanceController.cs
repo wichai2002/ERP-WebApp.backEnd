36c60d35744e5b834f646ccea9e0cd366f4fa491
wichai2002 <kommongkhun.wichai@gmail.com>
2024-03-07 23:30:17 +0700

    d

--- Yinnaxs_BackEnd/Controllers/AttendanceController.cs
+++ Yinnaxs_BackEnd/Controllers/AttendanceController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Yinnaxs_BackEnd.Context;
 using Yinnaxs_BackEnd.Models;
